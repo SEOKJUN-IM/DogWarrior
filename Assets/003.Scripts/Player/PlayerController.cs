7fc5dd94b296ffb86a039c6a3876eea2eb567d3b
Seokjun Im <carnivorous5@gmail.com>
2025-05-07 17:25:50 +0900

    [feat] 대쉬 추가

--- Assets/003.Scripts/Player/PlayerController.cs
+++ Assets/003.Scripts/Player/PlayerController.cs
@@ -3,11 +3,19 @@ using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{
+{    
     [Header("Movement")]    
     public float rotationSpeed;
     public Transform body;
     public NavMeshAgent agent;
+
+    [Header("Dash")]
+    public float dashDistance;    
+    
+    private Vector3 dashPoint;
+    private bool isDash = false;
+    private float dashTime = 0f;
+    private float lastDashTime;
 
     public Animator animator;       
 
@@ -18,12 +26,12 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Move();
+        Move();        
     }
 
     void Update()
     {
-        
+        Dash();
     }
 
     void Move()
@@ -44,6 +52,8 @@ public class PlayerController : MonoBehaviour
     {        
         if (context.phase == InputActionPhase.Performed)
         {
+            agent.isStopped = false;
+
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) agent.SetDestination(hit.point);            
         }
@@ -54,16 +64,51 @@ public class PlayerController : MonoBehaviour
     {
         if (context.phase == InputActionPhase.Performed)
         {
-            agent.SetDestination(body.transform.position);
+            agent.isStopped = true;            
+            animator.SetTrigger("onAttack");            
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                Vector3 dir = new Vector3(hit.point.x - body.transform.position.x, 0f, hit.point.z - body.transform.position.z);
+                body.transform.forward = dir;                
+            }            
+        }
+    }
+
+    // InputAction Dash
+    public void Dash()
+    {
+        if (isDash)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, dashPoint, Time.deltaTime * 15f);
+            animator.SetBool("isMove", true);
+            dashTime += Time.deltaTime;            
+        }
+
+        if (Vector3.Distance(transform.position, dashPoint) < 0.05f || dashTime >= 0.75f)
+        {            
+            dashTime = 0f;
+            isDash = false;
+            animator.SetBool("isMove", false);
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            agent.isStopped = true;
 
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
                 Vector3 dir = new Vector3(hit.point.x - body.transform.position.x, 0f, hit.point.z - body.transform.position.z);
                 body.transform.forward = dir;
-            }
 
-            animator.SetTrigger("onAttack");
+                dashPoint = transform.position + dashDistance * dir.normalized;
+                isDash = true;
+            }
         }
     }
 }
